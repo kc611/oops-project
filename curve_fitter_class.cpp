2c91da1a43306efda210e181fb2629875ec5753f
kc611 <ckaustubhm06@gmail.com>
2020-11-04 09:01:07 +0530

    Added more functions

--- curve_fitter_class.cpp
+++ curve_fitter_class.cpp
@@ -1,9 +1,7 @@
 #include<iostream>
-#include<vector>
 #include<limits.h>
-#include "function_class.cpp"
 using namespace std;
-#define matrix_size 1000
+#define matrix_size 100 // for some reason matrix size 1000 will not execute memory overflow perhaps
 
 class curve_fitter{
     private:
@@ -13,6 +11,7 @@ class curve_fitter{
         double jacobian_matrix[matrix_size][matrix_size];
         double jacobian_matrix_transpose[matrix_size][matrix_size];
         double multi_matrix[matrix_size][matrix_size];
+        double inverse_matrix[matrix_size][matrix_size];
         double residual_array[matrix_size];
         double x_data_points[matrix_size];
         double y_data_points[matrix_size];
@@ -27,19 +26,25 @@ class curve_fitter{
                 y_data_points[i]=0;
                 for(int j=0;j<matrix_size;j++){
                     jacobian_matrix[i][j]=0;
+                    multi_matrix[i][j]=0;
+                    jacobian_matrix_transpose[i][j]=0;
+                    inverse_matrix[i][j]=0;
                 }
                 parameters[i]=1;
             }
+            return 0;
         }
 
         bool show_graph(){
-
+            return 0;
         }
 
         bool calc_residuals(){
             for(int i=0;i<num_data_points;i++){
                 residual_array[i] = curr_function(x_data_points[i],parameters) - y_data_points[i];
             }
+            
+            return 0;
         }
 
         bool eval_jacobian(){
@@ -53,6 +58,7 @@ class curve_fitter{
                     jacobian_matrix_transpose[j][i]=jacobian_matrix[i][j];
                 }
             }
+            return 0;
 
         }
 
@@ -60,38 +66,76 @@ class curve_fitter{
             for(int i=0;i<num_data_points;i++){
                 for(int j=0;j<num_data_points;j++){
                     for(int k=0;k<num_parameters;k++){
-                        multi_matrix[i][j]=jacobian_matrix[i][k]+jacobian_matrix_transpose[k][j];
+                        multi_matrix[i][j]+=jacobian_matrix[i][k]*jacobian_matrix_transpose[k][j];
                     }
                 }
             }
 
-            for(int i=0;i<num_data_points;i++){
-                for(int j=0;j<num_data_points;j++){
-                    cout<<multi_matrix[i][j]<<" ";
-                }
+            return 0;
+        }
+
+        double get_determinant(int curr_size,double curr_matrix[][matrix_size]){
+            double curr_determinanat = 0;
+            if(curr_size==1){
+                return curr_matrix[0][0];
             }
+            for(int i=0;i<curr_size;i++){
+                double curr_element = curr_matrix[i][0];
+                double child_matrix[matrix_size][matrix_size];
+                int kpointer=0,jpointer=0;
+                for(int j=0;j<curr_size;j++){
+                    if(j==i){
+                            continue;
+                    }
+                    for(int k=0;k<curr_size;k++){
+                        if(k==i){
+                            continue;
+                        }
+                        child_matrix[jpointer][kpointer]=curr_matrix[j][k];kpointer++;
+                    }
+                    jpointer++;
+                }
+                curr_determinanat+=curr_element*get_determinant(curr_size-1,child_matrix);
+            }    
+            return curr_determinanat;
+        }
+
+        double* get_adjoint(int curr_size,double curr_matrix[matrix_size][matrix_size]){
+
+        }
+
+        bool calculate_inverse(){
+            double determinant = get_determinant(num_data_points,multi_matrix);
+            double *adjoint = get_adjoint(num_data_points,multi_matrix);
+	        cout<<determinant<<endl;
+            // for(int i=0;i<num_data_points;i++){
+            //     for(int j=0;j<num_data_points;j++){
+            //         cout<<inverse_matrix[i][j]<<" ";
+            //     }
+            //     cout<<endl;
+            // }
+            return 0;
         }
 
         bool update_parameters(){
+            multiply_matrices();
+            calculate_inverse();
 
+            return 0;
         }
 
         bool update_graph(){
-
+            return 0;
         }
     public:
         // This constructor would take argument as data points and assign default value to matrix and residuals
-        curve_fitter(vector<double> x_data,vector<double> y_data){
+        curve_fitter(int datapoints,double* x_data,double* y_data){
 
-            if(x_data.size()!=y_data.size()){
-                //Invalid data error
-            }else{
-                num_data_points = x_data.size();
-            }
+            num_data_points = datapoints;
 
             initialize_curve_fitter();
-
-            for(int i=0;i<x_data.size();i++){
+            
+            for(int i=0;i<datapoints;i++){
                 x_data_points[i] = x_data[i];
                 y_data_points[i] = y_data[i];
             }
@@ -125,6 +169,7 @@ class curve_fitter{
             }
 
             // return 1 if opreation successful 0 if failed
+            return 0;
         }
 
     
