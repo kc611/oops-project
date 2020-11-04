cee6b01ccd1f3307cc555302113e1b621bf84442
kc611 <ckaustubhm06@gmail.com>
2020-11-04 12:20:10 +0530

    Added few functions

--- curve_fitter_class.cpp
+++ curve_fitter_class.cpp
@@ -8,11 +8,17 @@ using namespace std;
 class curve_fitter{
     private:
         int num_data_points;
+        int num_parameters;
+        int num_iterations;
         double jacobian_matrix[matrix_size][matrix_size];
+        double jacobian_matrix_transpose[matrix_size][matrix_size];
+        double multi_matrix[matrix_size][matrix_size];
         double residual_array[matrix_size];
         double x_data_points[matrix_size];
         double y_data_points[matrix_size];
-        function_object curr_function;
+        double (*curr_function)(double,double*);
+        double parameters[matrix_size];
+        double step_value;
 
         bool initialize_curve_fitter(){
             for(int i=0;i<matrix_size;i++){
@@ -22,6 +28,7 @@ class curve_fitter{
                 for(int j=0;j<matrix_size;j++){
                     jacobian_matrix[i][j]=0;
                 }
+                parameters[i]=1;
             }
         }
 
@@ -30,11 +37,39 @@ class curve_fitter{
         }
 
         bool calc_residuals(){
-
+            for(int i=0;i<num_data_points;i++){
+                residual_array[i] = curr_function(x_data_points[i],parameters) - y_data_points[i];
+            }
         }
 
         bool eval_jacobian(){
+            for(int i=0;i<num_data_points;i++){
+                double f_x = curr_function(x_data_points[i],parameters); 
+                for(int j=0;j<num_parameters;j++){
+                    parameters[j]+=step_value;
+                    double f_x_h = curr_function(x_data_points[i],parameters);
+                    parameters[j]-=step_value;
+                    jacobian_matrix[i][j]=f_x_h/step_value - f_x/step_value;
+                    jacobian_matrix_transpose[j][i]=jacobian_matrix[i][j];
+                }
+            }
 
+        }
+
+        bool multiply_matrices(){
+            for(int i=0;i<num_data_points;i++){
+                for(int j=0;j<num_data_points;j++){
+                    for(int k=0;k<num_parameters;k++){
+                        multi_matrix[i][j]=jacobian_matrix[i][k]+jacobian_matrix_transpose[k][j];
+                    }
+                }
+            }
+
+            for(int i=0;i<num_data_points;i++){
+                for(int j=0;j<num_data_points;j++){
+                    cout<<multi_matrix[i][j]<<" ";
+                }
+            }
         }
 
         bool update_parameters(){
@@ -64,15 +99,18 @@ class curve_fitter{
         }
 
         // This must take in function as an argument (function() from main in this case) and create a function object from it
-        bool fit_curve(int number_of_interations){
+        bool fit_curve(double (*function)(double,double*),int number_of_parameters,int number_of_iterations,double step){
             // create a function ibject and assign it to curr function
-
+            curr_function = function;
+            step_value = step;
+            num_parameters = number_of_parameters;
+            num_iterations = number_of_iterations;
             // This is optional
             show_graph();
 
-            for(int i=1;i<=number_of_interations;i++){
+            for(int i=1;i<=number_of_iterations;i++){
                 //for i'th iteration
-
+                
                 // calculate residual array
                 calc_residuals();
 
--- main.cpp
+++ main.cpp
@@ -3,14 +3,16 @@
 #include<vector>
 #include "io_manager.cpp"
 #include "curve_fitter_class.cpp"
-
 using namespace std;
 
 bool import_from_file = 0;
 int data_size;
 
-double function(double x,double a,double b){
-    double y = a*x+b; 
+double user_function(double x,double parameters[]){
+    double a = parameters[0];double b = parameters[1];
+
+    double y = a*x+b;
+    
     return y;
 }
 
@@ -37,12 +39,12 @@ int main(){
         y_data_points.push_back(5);
         // datapoints are (1,1),(2,2)... and equation is of form ax+b so our final output should give a=1 and b=0
     }
-
     //curve fitter class constructor called and data points passed to it.
     curve_fitter example_curve_fitter = curve_fitter(x_data_points,y_data_points);
 
     // somehow pass function here as argument.
-    example_curve_fitter.fit_curve(10);
+    example_curve_fitter.fit_curve(&user_function,2,1,0.0001);
 
+    cout<<"DONE"<<endl;
 
 }
